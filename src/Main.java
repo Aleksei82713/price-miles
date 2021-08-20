3f8e177c52447c0238c0f6dede43a9fad6443e44
Aleksei Ilin <li0810@mail.ru>
2021-08-20 17:58:11 +0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public  class  Main {
+    public  static  void  main ( String [] args ) {
+        int price =  7000 ;
+        int miles = 20;
+        int calculation = price/miles;
+        System.out.println(calculation);
+
+    }
+}
+
